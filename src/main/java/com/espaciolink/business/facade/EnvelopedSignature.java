b071176630eac0159c428173dedfb291e853de5b
Xavier Ramirez <xavier@localhost.localdomain>
2013-10-04 15:35:16 -0400

    firma xml

---
+++ src/main/java/com/espaciolink/business/facade/EnvelopedSignature.java
@@ -0,0 +1,78 @@
+package com.espaciolink.business.facade;
+
+import javax.ejb.Stateless;
+
+import org.w3c.dom.Document;
+
+import es.mityc.firmaJava.libreria.xades.DataToSign;
+import es.mityc.firmaJava.libreria.xades.XAdESSchemas;
+import es.mityc.javasign.EnumFormatoFirma;
+import es.mityc.javasign.xml.refs.AllXMLToSign;
+import es.mityc.javasign.xml.refs.ObjectToSign;
+
+/**
+ * <p>
+ * Clase de ejemplo para la firma XAdES-BES enveloped de un documento. El
+ * ejemplo firmará el recurso indicado en la constante
+ * <code>RESOURCE_TO_SIGN</code> y el nombre del fichero resultante será el
+ * indicado por la constante <code>SIGN_FILE_NAME</code>.
+ * </p>
+ * <p>
+ * Para realizar la firma se utilizará el almacén PKCS#12 definido en la
+ * constante <code>GenericXMLSignature.PKCS12_FILE</code>, al que se accederá
+ * mediante la password definida en la constante
+ * <code>GenericXMLSignature.PKCS12_PASSWORD</code>. El directorio donde quedará
+ * el archivo XML resultante será el indicado en al constante
+ * <code>GenericXMLSignature.OUTPUT_DIRECTORY</code>
+ * </p>
+ * 
+ */
+@Stateless
+public class EnvelopedSignature extends GenericXMLSignature {
+
+	/**
+	 * Recurso a firmar
+	 */
+//	private final static String RESOURCE_TO_SIGN = "com/espaciolink/business/facade/factura.xml";
+	private final static String RESOURCE_TO_SIGN = "factura.xml";
+
+	/**
+	 * <p>
+	 * Fichero donde se desea guardar la firma
+	 * </p>
+	 */
+	private final static String SIGN_FILE_NAME = "factura-firmada.xml";
+
+	/**
+	 * <p>
+	 * Punto de entrada al programa
+	 * </p>
+	 * 
+	 * @param args
+	 *            Argumentos del programa
+	 */
+	public void sign() {
+		EnvelopedSignature signature = new EnvelopedSignature();
+		signature.execute();
+
+	}
+
+	@Override
+	protected DataToSign createDataToSign() {
+		DataToSign dataToSign = new DataToSign();
+		dataToSign.setXadesFormat(EnumFormatoFirma.XAdES_BES);
+		dataToSign.setEsquema(XAdESSchemas.XAdES_132);
+		dataToSign.setXMLEncoding("UTF-8");
+		dataToSign.setEnveloped(true);
+		Document docToSign = getDocument(RESOURCE_TO_SIGN);
+		dataToSign.setDocument(docToSign);
+		dataToSign.addObject(new ObjectToSign(new AllXMLToSign(),
+				"Documento de ejemplo", null, "text/xml", null));
+		return dataToSign;
+	}
+
+	@Override
+	protected String getSignatureFileName() {
+		return SIGN_FILE_NAME;
+	}
+}
--- src/main/java/com/espaciolink/presentation/rest/ElectronicInvoiceResource.java
+++ src/main/java/com/espaciolink/presentation/rest/ElectronicInvoiceResource.java
@@ -1,6 +1,7 @@
 package com.espaciolink.presentation.rest;
 
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import javax.ws.rs.Consumes;
@@ -10,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.espaciolink.business.facade.EnvelopedSignature;
 import com.espaciolink.persistence.enums.PermissionNameEnum;
 import com.espaciolink.security.AllowedPermission;
 import com.espaciolink.security.Guard;
@@ -20,16 +22,18 @@ import com.espaciolink.security.enums.PageInformationEnum;
 @Interceptors(Guard.class)
 public class ElectronicInvoiceResource {
 
-    
+    @EJB
+    EnvelopedSignature envelopedSignature;
 
    
 
     @POST
-    @Consumes(MediaType.TEXT_HTML)
+    @Consumes("*/*")
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/{institutionId}")
     @AllowedPermission(pageInformationEnum= PageInformationEnum.NEW, permissionNameEnum= PermissionNameEnum.GENERATE_PDF_INVOICE)
     public String postInvoice(String invoiceHeader, @PathParam("institutionId") String institutionId) {
+    	envelopedSignature.sign();
         return institutionId+" "+invoiceHeader;
     }
 }
